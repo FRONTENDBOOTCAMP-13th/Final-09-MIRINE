21a149dd69e8c67f021cdde04a072655e5615dad
Dongsusin <auroratime020715@gmail.com>
2025-07-14 17:17:47 +0900

    ✨ feat: feature/footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,4 +1,15 @@
 import styles from "./footer.module.css";
+
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.content}>
+        <img src="/footer/logo.svg" alt="로고" className={styles.logoImage} />
+        <div className={styles.info}>이용약관 | 개인정보처리방침</div>
+        <div className={styles.copy}>
+          Copyright ⓒ 2025 미리내 All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -37,156 +37,177 @@ export default function Header() {
   }, [pathname]);
 
   return (
-    <div
-      ref={wrapperRef}
-      className={`${styles.mobileWrapper} ${
-        isMobileMenuOpen ? styles.open : ""
-      }`}
-    >
-      <header className={styles.header}>
-        <div className={styles.navLeft}>
-          <ul className={styles.navList}>
-            <li>
-              <Link
-                href="/mirine"
-                className={pathname === "/mirine" ? styles.highlight : ""}
-              >
-                미리내
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/perfumes"
-                className={pathname === "/perfumes" ? styles.highlight : ""}
-              >
-                향수
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/aroma-shooter"
-                className={
-                  pathname === "/aroma-shooter" ? styles.highlight : ""
-                }
-              >
-                아로마슈터
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className={styles.logo}>
-          <Link href="/">
-            <img
-              src="/header/logo.svg"
-              alt="로고"
-              className={styles.logoImage}
-            />
-          </Link>
-        </div>
-
-        <div className={styles.navRight}>
-          <ul className={styles.navList}>
-            <li>
-              <Link
-                href="/reviews"
-                className={pathname === "/reviews" ? styles.highlight : ""}
-              >
-                리뷰
-              </Link>
-            </li>
-            <li>
-              <Link href="">로그인</Link>
-            </li>
-            <li>
-              <Link
-                href="/mypage"
-                className={pathname === "/mypage" ? styles.highlight : ""}
-              >
-                마이페이지
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/shopping-cart"
-                className={
-                  pathname === "/shopping-cart" ? styles.highlight : ""
-                }
-              >
-                장바구니
-              </Link>
-            </li>
-          </ul>
-
-          <button className={styles.hamburger} onClick={toggleMobileMenu}>
-            <img
-              src="/header/Icons.svg"
-              alt="메뉴 열기"
-              className={styles.hamburgerIcon}
-            />
-          </button>
-        </div>
-      </header>
-
+    <>
+      {isMobileMenuOpen && <div className={styles.backdrop}></div>}
       <div
-        className={`${styles.mobileMenu} ${
+        ref={wrapperRef}
+        className={`${styles.mobileWrapper} ${
           isMobileMenuOpen ? styles.open : ""
         }`}
       >
-        <ul>
-          <li>
-            <Link
-              href="/mirine"
-              className={pathname === "/mirine" ? styles.highlight : ""}
-            >
-              미리내
+        <header className={styles.header}>
+          <div className={styles.navLeft}>
+            <ul className={styles.navList}>
+              <li>
+                <Link
+                  href="/mirine"
+                  className={pathname === "/mirine" ? styles.highlight : ""}
+                >
+                  미리내
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/perfumes"
+                  className={pathname === "/perfumes" ? styles.highlight : ""}
+                >
+                  향수
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/aroma-shooter"
+                  className={
+                    pathname === "/aroma-shooter" ? styles.highlight : ""
+                  }
+                >
+                  아로마슈터
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.logo}>
+            <Link href="/">
+              <img
+                src="/header/logo.svg"
+                alt="로고"
+                className={styles.logoImage}
+              />
             </Link>
-          </li>
-          <li>
-            <Link
-              href="/perfumes"
-              className={pathname === "/perfumes" ? styles.highlight : ""}
-            >
-              향수
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/aroma-shooter"
-              className={pathname === "/aroma-shooter" ? styles.highlight : ""}
-            >
-              아로마슈터
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/reviews"
-              className={pathname === "/reviews" ? styles.highlight : ""}
-            >
-              리뷰
-            </Link>
-          </li>
-          <li>
-            <Link href="">로그인</Link>
-          </li>
-          <li>
-            <Link
-              href="/mypage"
-              className={pathname === "/mypage" ? styles.highlight : ""}
-            >
-              마이페이지
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/shopping-cart"
-              className={pathname === "/shopping-cart" ? styles.highlight : ""}
-            >
-              장바구니
-            </Link>
-          </li>
-        </ul>
+          </div>
+
+          <div className={styles.navRight}>
+            <ul className={styles.navList}>
+              <li>
+                <Link
+                  href="/reviews"
+                  className={pathname === "/reviews" ? styles.highlight : ""}
+                >
+                  리뷰
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/login"
+                  className={pathname === "/login" ? styles.highlight : ""}
+                >
+                  로그인
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/mypage"
+                  className={pathname === "/mypage" ? styles.highlight : ""}
+                >
+                  마이페이지
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/shopping-cart"
+                  className={
+                    pathname === "/shopping-cart" ? styles.highlight : ""
+                  }
+                >
+                  장바구니
+                </Link>
+              </li>
+            </ul>
+
+            <button className={styles.hamburger} onClick={toggleMobileMenu}>
+              <img
+                src="/header/Icons.svg"
+                alt="메뉴 열기"
+                className={styles.hamburgerIcon}
+              />
+            </button>
+          </div>
+        </header>
+
+        <div
+          className={`${styles.mobileMenu} ${
+            isMobileMenuOpen ? styles.open : ""
+          }`}
+        >
+          <ul>
+            <div className={styles.mobile_top}>
+              <li>
+                <Link
+                  href="/mirine"
+                  className={pathname === "/mirine" ? styles.highlight : ""}
+                >
+                  미리내
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/perfumes"
+                  className={pathname === "/perfumes" ? styles.highlight : ""}
+                >
+                  향수
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/aroma-shooter"
+                  className={
+                    pathname === "/aroma-shooter" ? styles.highlight : ""
+                  }
+                >
+                  아로마슈터
+                </Link>
+              </li>
+            </div>
+            <div className={styles.mobile_bottom}>
+              <li>
+                <Link
+                  href="/reviews"
+                  className={pathname === "/reviews" ? styles.highlight : ""}
+                >
+                  리뷰
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/login"
+                  className={pathname === "/login" ? styles.highlight : ""}
+                >
+                  로그인
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/mypage"
+                  className={pathname === "/mypage" ? styles.highlight : ""}
+                >
+                  마이페이지
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/shopping-cart"
+                  className={
+                    pathname === "/shopping-cart" ? styles.highlight : ""
+                  }
+                >
+                  장바구니
+                </Link>
+              </li>
+            </div>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
