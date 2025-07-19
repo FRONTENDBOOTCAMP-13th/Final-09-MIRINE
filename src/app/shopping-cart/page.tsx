2c1ecac4e9b44654c249253e5440a113216e4575
seuchoi0531 <seuchoi0531@gmail.com>
2025-07-19 22:15:07 +0900

    ✨ feat: 장바구니 파일 분리 세분화 및 태그명 변경, 공통 마크업 스타일 분리

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -1,11 +1,22 @@
-import CartWithItems from '@/app/shopping-cart/components/CartWithItems';
-import EmptyCart from '@/app/shopping-cart/components/EmptyCart';
+import CartWithItems from "@/app/shopping-cart/components/CartWithItems";
+import EmptyCart from "@/app/shopping-cart/components/EmptyCart";
+import styles from "./styles/common.module.css";
 
 export default function ShoppingCartPage() {
   return (
-    <div>
+    <div className={styles.shopping_cart}>
+      <h2 className={styles.header}>장바구니</h2>
+      <div className={styles.btn_group}>
+        <button type="button" className={styles.btn_delete}>
+          전체 선택
+        </button>
+        <button type="button" className={styles.btn_selection}>
+          선택 삭제
+        </button>
+      </div>
+
       <EmptyCart />
-      {/* <CartWithItems/> */}
+      {/* <CartWithItems /> */}
     </div>
   );
 }
