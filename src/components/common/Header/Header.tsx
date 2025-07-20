a478f5908ba88a68cb76d2799fd70519a24b0d3b
박상희 <9011155@naver.com>
2025-07-20 23:45:34 +0900

    🦁 chore: 클래스명 스네이크 방식으로 변경

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,221 +1,3 @@
-/* 
-"use client";
-import styles from "./header.module.css";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useState, useEffect, useRef } from "react";
-
-export default function Header() {
-  const pathname = usePathname();
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const wrapperRef = useRef<HTMLDivElement>(null);
-
-  const toggleMobileMenu = () => setIsMobileMenuOpen((prev) => !prev);
-
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (
-        wrapperRef.current &&
-        !wrapperRef.current.contains(event.target as Node)
-      ) {
-        setIsMobileMenuOpen(false);
-      }
-    }
-
-    if (isMobileMenuOpen) {
-      document.addEventListener("mousedown", handleClickOutside);
-    } else {
-      document.removeEventListener("mousedown", handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [isMobileMenuOpen]);
-
-  useEffect(() => {
-    setIsMobileMenuOpen(false);
-  }, [pathname]);
-
-  return (
-    <>
-      {isMobileMenuOpen && <div className={styles.backdrop}></div>}
-      <div
-        ref={wrapperRef}
-        className={`${styles.mobileWrapper} ${
-          isMobileMenuOpen ? styles.open : ""
-        }`}
-      >
-        <header className={styles.header}>
-          <div className={styles.navLeft}>
-            <ul className={styles.navList}>
-              <li>
-                <Link
-                  href="/mirine"
-                  className={pathname === "/mirine" ? styles.highlight : ""}
-                >
-                  미리내
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/perfumes"
-                  className={pathname === "/perfumes" ? styles.highlight : ""}
-                >
-                  향수
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/aroma-shooter"
-                  className={
-                    pathname === "/aroma-shooter" ? styles.highlight : ""
-                  }
-                >
-                  아로마슈터
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          <div className={styles.logo}>
-            <Link href="/">
-              <img
-                src="/header/logo.svg"
-                alt="로고"
-                className={styles.logoImage}
-              />
-            </Link>
-          </div>
-
-          <div className={styles.navRight}>
-            <ul className={styles.navList}>
-              <li>
-                <Link
-                  href="/reviews"
-                  className={pathname === "/reviews" ? styles.highlight : ""}
-                >
-                  리뷰
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/login"
-                  className={pathname === "/login" ? styles.highlight : ""}
-                >
-                  로그인
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/mypage"
-                  className={pathname === "/mypage" ? styles.highlight : ""}
-                >
-                  마이페이지
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/shopping-cart"
-                  className={
-                    pathname === "/shopping-cart" ? styles.highlight : ""
-                  }
-                >
-                  장바구니
-                </Link>
-              </li>
-            </ul>
-
-            <button className={styles.hamburger} onClick={toggleMobileMenu}>
-              <img
-                src="/header/Icons.svg"
-                alt="메뉴 열기"
-                className={styles.hamburgerIcon}
-              />
-            </button>
-          </div>
-        </header>
-
-        <div
-          className={`${styles.mobileMenu} ${
-            isMobileMenuOpen ? styles.open : ""
-          }`}
-        >
-          <ul>
-            <div className={styles.mobile_top}>
-              <li>
-                <Link
-                  href="/mirine"
-                  className={pathname === "/mirine" ? styles.highlight : ""}
-                >
-                  미리내
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/perfumes"
-                  className={pathname === "/perfumes" ? styles.highlight : ""}
-                >
-                  향수
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/aroma-shooter"
-                  className={
-                    pathname === "/aroma-shooter" ? styles.highlight : ""
-                  }
-                >
-                  아로마슈터
-                </Link>
-              </li>
-            </div>
-            <div className={styles.mobile_bottom}>
-              <li>
-                <Link
-                  href="/reviews"
-                  className={pathname === "/reviews" ? styles.highlight : ""}
-                >
-                  리뷰
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/login"
-                  className={pathname === "/login" ? styles.highlight : ""}
-                >
-                  로그인
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/mypage"
-                  className={pathname === "/mypage" ? styles.highlight : ""}
-                >
-                  마이페이지
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/shopping-cart"
-                  className={
-                    pathname === "/shopping-cart" ? styles.highlight : ""
-                  }
-                >
-                  장바구니
-                </Link>
-              </li>
-            </div>
-          </ul>
-        </div>
-      </div>
-    </>
-  );
-}
- */
-
-
-
 "use client"
 
 import Link from "next/link";
@@ -232,12 +14,9 @@ export default function Header() {
     router.back();
   }
 
-	// console.log(pathname);
-
 	useEffect(() => {
 		const sideNav = document.querySelector("#sideNav");
 		if(sideNav && sideNav.classList.contains(styles.open)){
-			// console.log(11);
 			sideNav.classList.remove(styles.open);
 		}
 	}, [pathname]);
@@ -247,12 +26,12 @@ export default function Header() {
 		<header className={styles.header}>
 			<div className={styles.inner}>
 				{(pathname === '/mirine' || pathname === '/perfumes') && (
-					<button onClick={handlePrev} className={styles.prevBtn}>
+					<button onClick={handlePrev} className={styles.prev_btn}>
 						<img src="/icon/Icon-prev.svg" alt="이전으로 이동"/>
 					</button>
 				)}
 
-				<ul className={styles.navLeft}>
+				<ul className={styles.nav_left}>
 					<li><Link href="/mirine" className={pathname === "/mirine" ? styles.active : ""}>미리내</Link></li>
 					<li><Link href="/perfumes" className={pathname === "/perfumes" ? styles.active : ""}>향수</Link></li>
 					<li><Link href="/aroma-shooter" className={pathname === "/aroma-shooter" ? styles.active : ""}>아로마슈터</Link></li>
@@ -260,30 +39,30 @@ export default function Header() {
 
 				<h1 className={styles.logo}>
 					<Link href="/">
-						<img className={styles.mLogo} src="/logo/logo-black-mobile.svg" alt="로고"/>
-						<img className={styles.pcLogo} src="/logo/logo-black-pc.svg" alt="로고"/>
+						<img className={styles.m_logo} src="/logo/logo-black-mobile.svg" alt="로고"/>
+						<img className={styles.pc_logo} src="/logo/logo-black-pc.svg" alt="로고"/>
 					</Link>
 				</h1>
 
-				<ul className={styles.navRight}>
+				<ul className={styles.nav_right}>
 					<li><Link href="/reviews" className={pathname === "/reviews" ? styles.active : ""}>리뷰</Link></li>
 					<li><Link href="/login" className={pathname === "/login" ? styles.active : ""}>로그인</Link></li>
 					<li><Link href="/mypage" className={pathname === "/mypage" ? styles.active : ""}>마이페이지</Link></li>
 					<li><Link href="/shopping-cart" className={pathname === "/shopping-cart" ? styles.active : ""}>장바구니</Link><span className={styles.cartCnt}>0</span></li>
 				</ul>
 
-				<button className={styles.sideNavBtn} onClick={() => document.querySelector(`.${styles.sideNav}`)?.classList.toggle(styles.open)}>
+				<button className={styles.side_nav_btn} onClick={() => document.querySelector(`.${styles.side_nav}`)?.classList.toggle(styles.open)}>
 					<img src="/icon/Icon-right-toggle-menu.svg" alt="모바일 토글 메뉴"/>
 				</button>
 			</div>
 
-			<div id="sideNav" className={styles.sideNav}>
-				<ul className={styles.mainMenu}>
+			<div id="sideNav" className={styles.side_nav}>
+				<ul className={styles.main_menu}>
 					<li><Link href="/mirine" className={pathname === "/mirine" ? styles.active : ""}>미리내</Link></li>
 					<li><Link href="/perfumes" className={pathname === "/perfumes" ? styles.active : ""}>향수</Link></li>
 					<li><Link href="/aroma-shooter" className={pathname === "/aroma-shooter" ? styles.active : ""}>아로마슈터</Link></li>
 				</ul>
-				<ul className={styles.subMenu}>
+				<ul className={styles.sub_menu}>
 					<li><Link href="/reviews" className={pathname === "/reviews" ? styles.active : ""}>리뷰</Link></li>
 					<li><Link href="/login" className={pathname === "/login" ? styles.active : ""}>로그인</Link></li>
 					<li><Link href="/mypage" className={pathname === "/mypage" ? styles.active : ""}>마이페이지</Link></li>
