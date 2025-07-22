21e41525e5852ed5b66e79a4de71a50987425a94
hongyerin <yerinhong00@gmail.com>
2025-07-22 15:30:51 +0900

    ✨ feat: 테스트 결과 페이지 수정 및 icon 추가

--- src/components/MinineTest/ResultPage/PerfumeResult.tsx
+++ src/components/MinineTest/ResultPage/PerfumeResult.tsx
@@ -1,5 +1,19 @@
-export default function PerfumeResult (){
+import styles from "@/components/MinineTest/ResultPage/perfumeResult.module.css"
+import Image from "next/image";
+
+export default function PerfumeResult(){
   return(
-    <>kk</>
+    <div className={styles.result}>
+      {/* 향수 결과 정보 */}
+      <div className={styles.item}>
+        <Image src="/" alt="향수 이미지" width="40" height="40" className={styles.item_img} />
+        <div className={styles.item_info}>
+          <p className={styles.brand_name}>브랜드</p>
+          <p className={styles.product_name}>향수이름</p>
+        </div>
+        {/* 바로가기 버튼 */}
+        <button type="submit" className={styles.btn}>바로가기</button>
+      </div>
+    </div>
   )
 }
