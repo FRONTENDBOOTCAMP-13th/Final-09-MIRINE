75dbbb982868fed0fff32f6fffef09c2a46f6c8d
hongyerin <yerinhong00@gmail.com>
2025-07-22 11:46:20 +0900

    💄 style: 미리내 테스트 결과 페이지 수정

--- src/app/mirine-test/page.tsx
+++ src/app/mirine-test/page.tsx
@@ -1,13 +1,15 @@
 import MirineTest from "@/components/MinineTest/TestPage/MirineTest";
 import TestStartPage from "@/components/MinineTest/StartPage/TestStartPage";
 import TestHeader from "@/components/MinineTest/Header/Header";
+import ResultPage from "@/components/MinineTest/ResultPage/ResultPage";
 
 export default function MirineTestPage() {
   return (
     <>
-      <TestHeader />
+      {/* <TestHeader /> */}
       {/* <TestStartPage /> */}
-      <MirineTest />
+      {/* <MirineTest /> */}
+      <ResultPage />
     </>
     
 );
