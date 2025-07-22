0bbe4c370cbc32bd685e2b61bfd9589a266c9f0e
hongyerin <yerinhong00@gmail.com>
2025-07-22 11:32:29 +0900

    ✨ feat: 미리내 테스트 헤더

--- src/app/mirine-test/page.tsx
+++ src/app/mirine-test/page.tsx
@@ -1,11 +1,13 @@
 import MirineTest from "@/components/MinineTest/TestPage/MirineTest";
 import TestStartPage from "@/components/MinineTest/StartPage/TestStartPage";
+import TestHeader from "@/components/MinineTest/Header/Header";
 
 export default function MirineTestPage() {
   return (
     <>
-      <TestStartPage />
-      {/* <MirineTest /> */}
+      <TestHeader />
+      {/* <TestStartPage /> */}
+      <MirineTest />
     </>
     
 );
