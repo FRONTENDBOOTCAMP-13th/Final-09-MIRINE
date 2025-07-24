a095cfb4a7b4f8ff0bfce32fa799170b68575dad
seuchoi0531 <seuchoi0531@gmail.com>
2025-07-24 15:12:03 +0900

    🐛 fix: 테스트 페이지에서 헤더와 푸터가 노출되지 않도록 수정

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,15 +1,20 @@
+"use client";
+import { usePathname } from "next/navigation";
 import styles from "./footer.module.css";
 
 export default function Footer() {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.content}>
-        <img src="/footer/logo.svg" alt="로고" className={styles.logoImage} />
-        <div className={styles.info}>이용약관 | 개인정보처리방침</div>
-        <div className={styles.copy}>
-          Copyright ⓒ 2025 미리내 All rights reserved.
+  const pathname = usePathname();
+
+  if (pathname === "/mirine-test") return;
+  else {
+    return (
+      <footer className={styles.footer}>
+        <div className={styles.content}>
+          <img src="/footer/logo.svg" alt="로고" className={styles.logoImage} />
+          <div className={styles.info}>이용약관 | 개인정보처리방침</div>
+          <div className={styles.copy}>Copyright ⓒ 2025 미리내 All rights reserved.</div>
         </div>
-      </div>
-    </footer>
-  );
+      </footer>
+    );
+  }
 }
