2dbd7e1c018a9bddf749b042c7590624382ea717
hongyerin <yerinhong00@gmail.com>
2025-07-25 09:10:22 +0900

    💄 style: 리뷰 마크업 수정

--- src/components/Review/ReviewCommon.tsx
+++ src/components/Review/ReviewCommon.tsx
@@ -3,16 +3,20 @@ import styles from "./reviewCommon.module.css";
 import Image from "next/image";
 
 export default function ReviewCommon(){
+  const itemCount = 123; 
+
   return(
     <div className={styles.common_section}>
       <h1 className={styles.title}>리뷰</h1>
+
       <div className={styles.btn_group}>
         <button className={styles.mirine}>미리내</button>
         <button className={styles.perfume}>향수</button>
         <button className={styles.aroma_shooter}>아로마슈터</button>
       </div>
-      <div className={styles.num_icon_section}>
-        <p className={styles.num}>총 n개</p>
+
+      <div className={styles.count_icon_section}>
+        <p className={styles.review_count}>총 {itemCount}개</p>
         <div className={styles.icon_group}>
          <button className={styles.list} type="button">
           <Image src="/icon/Icon-layout-list.svg" alt="리스트" width={18} height={14} />
