14d37128e41d62d997ade09ef99a18637068e6b1
hongyerin <yerinhong00@gmail.com>
2025-07-25 17:54:26 +0900

    💄 design: 버튼 색상 및 마크업 수정

--- src/components/Review/ReviewCommon.tsx
+++ src/components/Review/ReviewCommon.tsx
@@ -1,27 +1,65 @@
-// import Link from "next/link";
+'use client';
+import { useState } from "react";
 import styles from "./reviewCommon.module.css";
 import Image from "next/image";
 
 export default function ReviewCommon(){
+  const [active, setActive] = useState<"mirine"|"perfume"|"aroma">('mirine');
+  const count = 123;
+  
   return(
     <div className={styles.common_section}>
       <h1 className={styles.title}>리뷰</h1>
-      <div className={styles.btn_group}>
-        <button className={styles.mirine}>미리내</button>
-        <button className={styles.perfume}>향수</button>
-        <button className={styles.aroma_shooter}>아로마슈터</button>
-      </div>
-      <div className={styles.num_icon_section}>
-        <p className={styles.num}>총 n개</p>
+
+      <nav className={styles.btn_group}>
+        <ul className={styles.category_list}>
+          <li className={`${styles.category_btn} ${active === 'mirine' ? styles.active : ''}`} >
+            <button 
+              onClick={() => setActive('mirine')} 
+            >
+              미리내
+            </button>
+          </li>
+          <li className={`${styles.category_btn} ${active === 'perfume' ? styles.active : ''}`}>
+            <button 
+              onClick={() => setActive('perfume')} 
+            >
+              향수
+            </button>
+          </li>
+          <li className={`${styles.category_btn} ${active === 'aroma' ? styles.active : ''}`}>
+            <button 
+              onClick={() => setActive('aroma')} 
+            >
+              아로마슈터
+            </button>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.count_icon_section}>
+        <p className={styles.count}>총 {count}개</p>
         <div className={styles.icon_group}>
-         <button className={styles.list} type="button">
-          <Image src="/icon/Icon-layout-list.svg" alt="리스트" width={18} height={14} />
-         </button>
-         <button className={styles.grid} type="button">
-          <Image src="/icon/Icon-layout-grid.svg" alt="그리드" width={18} height={14} />  
-         </button> 
+          <ul className={styles.view_options}>
+            <li>
+              <button className={styles.view_btn} type="button" aria-label="리스트 보기">
+                <Image src="/icon/Icon-layout-list.svg" alt="리스트" width={18} height={14} />
+              </button>
+            </li>
+            <li>
+              <button className={styles.view_btn} type="button" aria-label="그리드 보기">
+                <Image src="/icon/Icon-layout-grid.svg" alt="그리드" width={18} height={14} />  
+              </button>
+            </li>
+          </ul>
         </div>
       </div>
+
+      <section className={styles.tab_content}>
+        {active === "mirine" && ('미리내 리뷰')}
+        {active === "perfume" && ('향수 리뷰')}
+        {active === "aroma" && ('아로마슈터 리뷰')}
+      </section>
     </div>
   )
 }
