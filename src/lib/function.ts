63180705d4ae3606c1f118c624b61a534eb6ceee
seuchoi0531 <seuchoi0531@gmail.com>
2025-07-28 03:58:56 +0900

    💡 comment: API GET 함수에 JSDoc 추가

--- src/lib/function.ts
+++ src/lib/function.ts
@@ -3,6 +3,11 @@ const URL = process.env.OPEN_MARKET_URL;
 const CLIENT_ID = process.env.CLIENT_ID;
 
 /* 향수 관련 함수 */
+/**
+ * GET /products
+ * @description 제품 전체를 DB에서 가져오는 함수
+ * @returns 제품 전체 데이터를 결과로 가지는 Promise
+ */
 export async function getAllPerfumes() {
   try {
     const res = await fetch(`${URL}/products`, {
@@ -18,6 +23,12 @@ export async function getAllPerfumes() {
   }
 }
 
+/**
+ * GET /products/{_id}
+ * @description 제품 한 개를 DB에서 가져오는 함수
+ * @param {number} id 제품의 _id
+ * @returns 제품 한 개 데이터를 결과로 가지는 Promise
+ */
 export async function getPerfume(id: number) {
   try {
     const res = await fetch(`${URL}/products/${id}`, {
@@ -34,6 +45,11 @@ export async function getPerfume(id: number) {
 }
 
 /* 리뷰 관련 함수 */
+/**
+ * GET /replies/all
+ * @description 리뷰 전체를 DB에서 가져오는 함수
+ * @returns 리뷰 전체 데이터를 결과로 가지는 Promise
+ */
 export async function getAllReivews() {
   try {
     const res = await fetch(`${URL}/replies/all`, {
@@ -49,6 +65,12 @@ export async function getAllReivews() {
   }
 }
 
+/**
+ * GET /replies
+ * @description 로그인 한 사용자의 리뷰를 DB에서 가져오는 함수
+ * @param {string} token 로그인 한 사용자에게 부여되는 Access Token 
+ * @returns 로그인 한 사용자의 리뷰 데이터를 결과로 가지는 Promise
+ */
 export async function getMyReivews(token: string) {
   try {
     const res = await fetch(`${URL}/replies`, {
@@ -65,6 +87,13 @@ export async function getMyReivews(token: string) {
   }
 }
 
+
+/**
+ * GET /replies/{_id}
+ * @description 리뷰 한 개를 DB에서 가져오는 함수
+ * @param {id} id 리뷰의 _id
+ * @returns 리뷰 한 개 데이터를 결과로 가지는 Promise
+ */
 export async function getReivew(id: number) {
   try {
     const res = await fetch(`${URL}/replies/${id}`, {
@@ -80,6 +109,12 @@ export async function getReivew(id: number) {
   }
 }
 
+/**
+ * GET /replies/products/${_id}
+ * @description 제품의 리뷰를 DB에서 가져오는 함수
+ * @param {number} id 제품의 _id
+ * @returns 제품의 리뷰 데이터를 결과로 가지는 Promise
+ */
 export async function getProductsReivews(id: number) {
   try {
     const res = await fetch(`${URL}/replies/products/${id}`, {
@@ -96,6 +131,12 @@ export async function getProductsReivews(id: number) {
 }
 
 /* 주문 관련 함수 */
+/**
+ * GET /orders
+ * @description 로그인 한 사용자의 구매 내역을 DB에서 가져오는 함수
+ * @param {string} token 로그인 한 사용자에게 부여되는 Access Token 
+ * @returns 로그인 한 사용자의 구매 내역 데이터를 결과로 가지는 Promise
+ */
 export async function getAllOrders(token: string) {
   try {
     const res = await fetch(`${URL}/orders`, {
@@ -113,6 +154,12 @@ export async function getAllOrders(token: string) {
 }
 
 /* 찜 관련 함수 */
+/**
+ * GET /bookmarks/product
+ * @description 로그인 한 사용자의 찜 목록을 DB에서 가져오는 함수
+ * @param {string} token 로그인 한 사용자에게 부여되는 Access Token 
+ * @returns 로그인 한 사용자의 찜 목록 데이터를 결과로 가지는 Promise
+ */
 export async function getAllLikes(token: string) {
   try {
     const res = await fetch(`${URL}/bookmarks/product`, {
@@ -130,6 +177,11 @@ export async function getAllLikes(token: string) {
 }
 
 /* 회원 관련 함수 */
+/**
+ * GET /users
+ * @description 사용자 전체를 DB에서 가져오는 함수
+ * @returns 사용자 전체 데이터를 결과로 가지는 Promise
+ */
 export async function getAllUsers() {
   try {
     const res = await fetch(`${URL}/users`, {
@@ -146,6 +198,11 @@ export async function getAllUsers() {
 }
 
 /* 미리내 테스트 관련 함수 */
+/**
+ * GET /posts
+ * @description 미리내 테스트 전체를 DB에서 가져오는 함수
+ * @returns 미리내 테스트 전체 데이터를 결과로 가지는 Promise
+ */
 export async function getAllMirineTests() {
   try {
     const res = await fetch(`${URL}/posts`, {
@@ -161,6 +218,12 @@ export async function getAllMirineTests() {
   }
 }
 
+/**
+ * GET /posts/users
+ * @description 로그인 한 사용자의 미리내 테스트를 DB에서 가져오는 함수
+ * @param {string} token 로그인 한 사용자에게 부여되는 Access Token 
+ * @returns 로그인 한 사용자의 미리내 테스트 데이터를 결과로 가지는 Promise
+ */
 export async function getMyMirineTests(token: string) {
   try {
     const res = await fetch(`${URL}/posts/users`, {
