962ff6159e1ee4755bca1ab6ca263e6478d4ea48
seuchoi0531 <seuchoi0531@gmail.com>
2025-07-28 05:27:54 +0900

    ✨ feat: 장바구니 스토어 구현

---
+++ src/store/shoppingCartStore.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+// 타입 지정해줘야 함
+const useShoppingCartStore = create(
+  persist(
+    (set, get) => ({
+      shoppingCart: [],
+      addItem: (item) => set((state) => ({ shoppingCart: [...state.shoppingCart, item] })),
+      deleteItem: (item) => set((state) => ({ shoppingCart: state.shoppingCart.filter((i) => i !== item) })),
+      resetShoppingCart: () => set({ shoppingCart: [] }),
+      getShoppingCart: () => get().shoppingCart,
+    }),
+    {
+      name: "shoppingCart",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
