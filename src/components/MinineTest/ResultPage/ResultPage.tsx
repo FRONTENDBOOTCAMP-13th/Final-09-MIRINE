4307a799ee780c28cf47988299f3f09d22523adc
hongyerin <yerinhong00@gmail.com>
2025-07-28 16:03:30 +0900

    💄 style: 테스트 결과 마크업 수정

--- src/components/MinineTest/ResultPage/ResultPage.tsx
+++ src/components/MinineTest/ResultPage/ResultPage.tsx
@@ -1,31 +1,44 @@
-import PerfumeResult from "@/components/MinineTest/ResultPage/PerfumeResult"
+import TestHeader from "@/components/MinineTest/Header/Header";
+import PerfumeResultList from "@/components/MinineTest/ResultPage/PerfumeResult/PerfumeResult";
 import styles from "@/components/MinineTest/ResultPage/resultPage.module.css"
 import Image from "next/image";
 
 export default function ResultPage() {
   return (
     <div className={styles.result_page}>
+      <TestHeader />
+      
       <h1 className={styles.title}>당신의 향수는?</h1>
 
       {/* 테스트 결과 */}
       <ul className={styles.result}>
-        <PerfumeResult />
-        <PerfumeResult />
-        <PerfumeResult />
+        <PerfumeResultList />
+        <PerfumeResultList />
+        <PerfumeResultList />
       </ul>
 
       <p className={styles.text}>{`*결과는 '미리내 테스트 결과'에서 다시 확인이 가능합니다.`}</p>
-
-      <div className={styles.btn}>
-        <button type="submit" className={styles.home_btn}>홈으로 이동하기</button>
-        <button type="submit" className={styles.again_btn}>테스트 다시하기</button>
+      
+      {/* 홈이동, 다시하기 버튼 */}
+      <div className={styles.btn_group}>
+        <button 
+          type="button" 
+          className={styles.home_btn}>홈으로 이동하기</button>
+        <button 
+          type="button" 
+          className={styles.again_btn}>테스트 다시하기</button>
       </div>
 
+      {/* 공유하기 */}
       <div className={styles.share}>
-        <button type="submit" className={styles.kakao}>
+        <button 
+          type="button" 
+          className={styles.kakao}>
           <Image src="/icon/Icon-kakao.svg" alt="링크" width="42" height="42" />
         </button>
-        <button type="submit" className={styles.link}>
+        <button 
+          type="button" 
+          className={styles.link}>
           <Image src="/icon/Icon-link5.svg" alt="링크" width="42" height="42" />
         </button>
       </div>
