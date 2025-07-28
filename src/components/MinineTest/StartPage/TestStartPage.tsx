82340dfb5e348febff511e776c6d746679183cf6
hongyerin <yerinhong00@gmail.com>
2025-07-28 14:22:16 +0900

    ✨ feat: 테스트 버튼 이동 코드 수정

--- src/components/MinineTest/StartPage/TestStartPage.tsx
+++ src/components/MinineTest/StartPage/TestStartPage.tsx
@@ -1,13 +1,32 @@
+'use client';
 import styles from "./testStartPage.module.css";
+import { useRouter } from 'next/navigation';
 
 export default function TestStartPage(){
+  const router = useRouter();
+
+  // 테스트 시작하기
+  const handleStartTest = () => {
+    router.push('/')
+  }
+  // 홈으로 이동하기
+  const handleGoHome = () => {
+    router.push('/');
+  }
+
   return(
     <div className={styles.test_start}>
-      <h1 className={styles.title}>당신의 향수는?</h1>
-      <div className={styles.btn_group}>
-        <button className={`${styles.btn} ${styles.btn_start}`} >테스트 시작하기</button>
-        <button className={`${styles.btn} ${styles.btn_home}`}>홈으로 이동하기</button>
-      </div>
+      <h2 className={styles.title}>당신의 향수는?</h2>
+      <nav className={styles.btn_group}>
+        <button 
+          type="button" 
+          className={`${styles.btn} ${styles.btn_start}`} 
+          onClick={handleStartTest} >테스트 시작하기</button>
+        <button 
+          type="button" 
+          className={`${styles.btn} ${styles.btn_home}`} 
+          onClick={handleGoHome}>홈으로 이동하기</button>
+      </nav>
     </div>
   );
 }
