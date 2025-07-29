34f9c8f0917967d6f4d2cc2501ae92a0da2b3968
hongyerin <yerinhong00@gmail.com>
2025-07-29 17:15:58 +0900

    ✨ feat: 저장 버튼 기능 추가

--- src/components/MypageAside/SaveButton/SaveButton.tsx
+++ src/components/MypageAside/SaveButton/SaveButton.tsx
@@ -1,12 +1,22 @@
 'use client'
 import styles from '@/components/MypageAside/SaveButton/saveButton.module.css'
-export default function SaveButton(){
+
+interface SaveButtonProps {
+  onClick?: () => void; //나중에 수정
+  disabled?: boolean;
+}
+
+export default function SaveButton({onClick, disabled = false}: SaveButtonProps){
   return(
     <div className={styles.btn}>
       <button 
+        type='button'
         className={styles.save_btn}
-        onClick={()=>{console.log('저장')}}
-      >저장</button>
+        onClick={onClick}
+        disabled={disabled}
+      >
+        저장
+      </button>
     </div>
   )
 }
