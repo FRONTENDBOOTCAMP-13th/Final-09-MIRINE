264383020d5e3d276d7e889c5e07cf3382f8377d
hongyerin <yerinhong00@gmail.com>
2025-07-30 09:29:45 +0900

    💄 style: 아이콘 수정

--- src/components/MypageAside/MypageAside.tsx
+++ src/components/MypageAside/MypageAside.tsx
@@ -8,9 +8,9 @@ export default function MypageAside() {
         </h2>
 
         <div className={styles.icon}>
-          <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M9 18L15 12L9 6" stroke="black" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg>
+          <svg width={12} height={12} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M9 18L15 12L9 6" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
         
         </div>
           <p className={styles.mypage_info}>내 정보</p>
@@ -19,7 +19,6 @@ export default function MypageAside() {
 
       <div className={styles.option}>
         <button
-          // onClick={}
           className={styles.option_select}
         >내 정보</button>
       </div>
