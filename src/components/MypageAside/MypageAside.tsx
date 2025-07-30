366eee05409b8bdf87407c05de4b654d4413fb12
hongyerin <yerinhong00@gmail.com>
2025-07-30 09:41:55 +0900

    💄 style: 마이페이지 아이콘 코드 수정

--- src/components/MypageAside/MypageAside.tsx
+++ src/components/MypageAside/MypageAside.tsx
@@ -9,7 +9,7 @@ export default function MypageAside() {
 
         <div className={styles.icon}>
           <svg width={12} height={12} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M9 18L15 12L9 6" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M9 18L15 12L9 6" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
         
         </div>
