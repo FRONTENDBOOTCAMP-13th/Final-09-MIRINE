11689cba6f758be154e172b147a9e3dc64f9bd74
hongyerin <yerinhong00@gmail.com>
2025-07-31 17:28:03 +0900

    ✨ feat: 구매하기 바텀시트

--- src/app/perfumes/[id]/page.tsx
+++ src/app/perfumes/[id]/page.tsx
@@ -1,9 +1,9 @@
-import DetailPage from "@/components/Mirine/MirineDetailPage/DetailPage";
+import PerfumeDetailPage from "@/components/perfumes/DetailPage/DetailPage";
 
 export default function PerfumeInfo() {
   return (
     <>
-      <DetailPage />
+      <PerfumeDetailPage />
     </>
   );
 }
