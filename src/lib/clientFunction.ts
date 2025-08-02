611cf60e67506ec56dd402184c44d6ae15ceeae3
seuchoi0531 <seuchoi0531@gmail.com>
2025-08-03 03:52:24 +0900

    ✨ feat: addPriceTemplate 함수 구현

--- src/lib/clientFunction.ts
+++ src/lib/clientFunction.ts
@@ -15,3 +15,19 @@ export function getAccessToken() {
 export function getUserID() {
   return localStorage.getItem("userId") || "";
 }
+
+export function addPriceTemplate(price: number) {
+  const priceStr = price.toString();
+  let str = "";
+  Array.from(priceStr).forEach((e, i) => {
+    str += e;
+    if (i !== priceStr.length - 1 && i % 3 === (priceStr.length - 1) % 3) str += ",";
+  });
+
+  return "￦" + str;
+}
+
+// 1234    0
+// 12345   1
+// 123456  2
+// 1234567 03
