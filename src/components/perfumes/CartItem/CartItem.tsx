30eeeb5c5635b1eb8366f137e9506cf441c4c709
seuchoi0531 <seuchoi0531@gmail.com>
2025-08-05 04:52:26 +0900

    ✨ feat: CartItem 컴포넌트 마크업/스타일링

---
+++ src/components/perfumes/CartItem/CartItem.tsx
@@ -0,0 +1,35 @@
+import { addml, addPriceTemplate } from "@/lib/clientFunction";
+import styles from "./cartItem.module.css";
+
+export default function CartItem({ data }: { data: { name: string; volume: number; quantity: number; price: number } }) {
+  return (
+    <li className={styles.cart_item}>
+      <div className={styles.top_section}>
+        <span>
+          {data.name} / {addml(data.volume)}
+        </span>
+        <span>
+          <svg width="15" height="15" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18 6.5L6 18.5M6 6.5L18 18.5" stroke="var(--black-500)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+        </span>
+      </div>
+      <div className={styles.bottom_section}>
+        <div className={styles.cnt_section}>
+          <button className={styles.cnt_dec_btn}>
+            <svg width="12" height="12" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5 12.5H19" stroke="var(--black-950)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+          </button>
+          <span className={styles.item_cnt}>{data.quantity}</span>
+          <button className={styles.cnt_inc_btn}>
+            <svg width="12" height="12" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M5 12.5H19M12 5.5V19.5" stroke="var(--black-950)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+          </button>
+        </div>
+        <span className={styles.item_price}>{addPriceTemplate(data.price)}</span>
+      </div>
+    </li>
+  );
+}
