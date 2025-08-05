ac22f91bfa7f6a4b4776a4e9073bd543f2cdca42
hongyerin <yerinhong00@gmail.com>
2025-08-05 10:32:04 +0900

    ✨ feat: 찜 리스트 로딩중 수정 및 테스트 폴더 추가

---
+++ src/app/devtest/actiontest/myLikeListTest/page.tsx
@@ -0,0 +1,11 @@
+import LikeListPage from "@/app/devtest/actiontest/myLikeListTest/MyLikeList";
+
+export default function Wishlist() {
+
+  return (
+    <>
+      <LikeListPage />
+    </>
+  )
+  ;
+}
