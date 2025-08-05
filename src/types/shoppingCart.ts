f01c140e1b9c2355541288bf685e7160fa54e7e8
seuchoi0531 <seuchoi0531@gmail.com>
2025-08-06 07:39:48 +0900

    ✨ feat: MirineState 인터페이스에 따른 CartState 인터페이스 변경

--- src/types/shoppingCart.ts
+++ src/types/shoppingCart.ts
@@ -1,10 +1,13 @@
 // shoppingCart
 // type이 m이면 향수아이디들
 // type이 p이면 향수아이디, 향수 이름, volume, quantity, price
+
+import { MirineItemInState } from "@/store/mirineStore";
+
 // 미리내 페이지 구현하다보면 type이 m일때의 배열이 달라질 수 있음
 export interface CartItemInStore {
   type: "m" | "p";
-  content: number[] | [number, string, number, number, number];
+  content: MirineItemInState[] | [number, string, number, number, number];
 }
 
 export interface CartState {
