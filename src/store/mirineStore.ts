51a3ec45b72da54ad68dd5cb7f94492d87dd7964
seuchoi0531 <seuchoi0531@gmail.com>
2025-08-06 07:39:19 +0900

    ✨ feat: zustand에서 쓰일 MirineState 인터페이스 정의

--- src/store/mirineStore.ts
+++ src/store/mirineStore.ts
@@ -1,13 +1,27 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
+export interface MirineItemInState {
+  id: number;
+  name: string;
+  path: string;
+  price: number;
+}
+
+export interface MirineState {
+  mirine: MirineItemInState[];
+  addItem: (item: MirineItemInState) => void;
+  deleteItem: (item: MirineItemInState) => void;
+  resetMirine: () => void;
+  getMirine: () => MirineItemInState[];
+}
 // 타입 지정해줘야 함
 const useMirineStore = create(
-  persist(
+  persist<MirineState>(
     (set, get) => ({
       mirine: [],
       addItem: (item) => set((state) => ({ mirine: [...state.mirine, item] })),
-      deleteItem: (item) => set((state) => ({ mirine: state.mirine.filter((i) => i !== item) })),
+      deleteItem: (item) => set((state) => ({ mirine: [...state.mirine.filter((i) => i.id !== item.id)] })),
       resetMirine: () => set({ mirine: [] }),
       getMirine: () => get().mirine,
     }),
