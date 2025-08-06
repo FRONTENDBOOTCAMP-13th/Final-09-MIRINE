0df5ef32dd2c732c36f09fd4ab50cbf833a04c6e
seuchoi0531 <seuchoi0531@gmail.com>
2025-08-06 23:13:15 +0900

    ✨ feat: zustand를 이용하여 장바구니에 상품을 보이도록 구현

--- src/components/ShoppingCart/CartControls/CartControls.tsx
+++ src/components/ShoppingCart/CartControls/CartControls.tsx
@@ -2,14 +2,24 @@
 import styles from "@/components/ShoppingCart/CartControls/cartControls.module.css";
 import { SmallButton } from "@/components/ui/Button/SmallButton";
 
-export default function CartControls() {
+export default function CartControls({ checkedArray, checkedArrayHandle, deleteItemInShoppingCart }: { checkedArray; checkedArrayHandle: () => void; deleteItemInShoppingCart: () => void }) {
   return (
     <div className={styles.btn_group}>
-      <SmallButton ButtonColor="black" onClick={() => console.log("전체 선택")}>
-        전체 선택
+      <SmallButton
+        ButtonColor="black"
+        onClick={() => {
+          checkedArrayHandle();
+        }}
+      >
+        전체 {checkedArray.every((e) => e) ? "해제" : "선택"}
       </SmallButton>
 
-      <SmallButton ButtonColor="orange" onClick={() => console.log("선택 삭제")}>
+      <SmallButton
+        ButtonColor="orange"
+        onClick={() => {
+          deleteItemInShoppingCart();
+        }}
+      >
         선택 삭제
       </SmallButton>
     </div>
