3167f1896ef41c85368cd935d9fc0e3993486d0d
seuchoi0531 <seuchoi0531@gmail.com>
2025-08-06 16:51:16 +0900

    ✨ feat: 회원 zustand 기능 구현

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,20 +1,38 @@
+import { UserState } from "@/types/user";
 import { create } from "zustand";
-import { createJSONStorage, persist } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 
 // 타입 지정해줘야 함
-const useUserStore = create(
+const useUserStore = create<UserState>()(
   persist(
     (set, get) => ({
       user: null,
-      setUser: (user) => set({ user }),
+      setUser: (user) => set({ user: { ...user } }),
       resetUser: () => set({ user: null }),
-      getUser: () => get(),
+      getUser: () => get().user,
     }),
     {
-      name: "accessToken",
-      storage: createJSONStorage(() => localStorage),
+      name: "user",
+      partialize: (state) => ({
+        token: state.user?.token.accessToken,
+        userID: state.user?._id,
+      }),
     }
   )
 );
+// const useUserStore = create(
+//   persist<UserState>(
+//     (set, get) => ({
+//       user: null,
+//       setUser: (user) => set({ user }),
+//       resetUser: () => set({ user: null }),
+//       getUser: () => get().user,
+//     }),
+//     {
+//       name: "user",
+//       storage: createJSONStorage(() => localStorage),
+//     }
+//   )
+// );
 
 export default useUserStore;
