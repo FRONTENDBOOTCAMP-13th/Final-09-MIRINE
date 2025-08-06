c9a49a2f4064b5d7525beaf20b5e28cd3eba0c41
seuchoi0531 <seuchoi0531@gmail.com>
2025-08-06 14:14:29 +0900

    🐛 fix: 미리내 테스트 페이지에 이전으로 이동하는 아이콘 제거

--- src/components/MinineTest/Header/Header.tsx
+++ src/components/MinineTest/Header/Header.tsx
@@ -2,32 +2,20 @@
 import Link from "next/link";
 import styles from "./header.module.css";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 
-export default function TestHeader(){
-  const router = useRouter();
-  const handlePrev = () => {
-    router.back();
-  }
-
+export default function TestHeader() {
   return (
-    <header className={styles.header} >
-			<button onClick={handlePrev} className={styles.prev_btn} type="submit">
-				<Image src="/icon/Icon-prev.svg" alt="이전으로 이동" width="24" height="24" />
-			</button>
-
+    <header className={styles.header}>
       <h1 className={styles.logo}>
         <Link href="/">
           <Image className={styles.m_logo} src="/logo/logo-black-mobile.svg" alt="로고" width="66" height="16" />
           {/* <Image className={styles.pcLogo} src="/logo/logo-black-pc.svg" alt="로고" width="115" height="28" /> */}
-				</Link>
+        </Link>
       </h1>
 
-      <button type="submit">
-        <Link href="/">
-          <Image className={styles.home_logo} src="/icon/Icon-home.svg" alt="모바일 홈" width="24" height="24" />
-        </Link>
-      </button>
+      <Link href="/" className={styles.home_logo}>
+        <Image src="/icon/Icon-home.svg" alt="모바일 홈" width="24" height="24" />
+      </Link>
     </header>
   );
 }
